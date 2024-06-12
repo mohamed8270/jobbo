7a420a7f05860a76a32fc9b03c4e71e3a7007056
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-12 16:19:04 +0530

    tailwindcss config and home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import HomePage from "@/src/views/HomePage";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+    <div className='bg-jgreen h-screen w-screen'>
+        Home Page
+    </div>
+    </>
   )
 }
 
