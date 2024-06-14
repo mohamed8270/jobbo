a6aacb78fa5508915f2ba6afd4d069666bfcd337
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-14 16:47:53 +0530

    home page with search bar completed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import React,{ useState} from 'react'
 import SearchBarRepo from './interface/SearchBarRepo';
 
 const HeroSection = () => {
-    const img = 'https://img.freepik.com/free-vector/woman-men-teamwork-marketing-corporate_24877-54763.jpg?t=st=1718355181~exp=1718358781~hmac=146955ba629307818629c587a2f3fb43e559cd57e075ec4acc0e8c72cf48c479&w=740';
+    const img = 'https://cdni.iconscout.com/illustration/premium/thumb/job-interview-4462381-3702651.png';
 
     const [userJobInput, setuserJobInput] = useState('');
 
@@ -17,12 +17,12 @@ const HeroSection = () => {
     <div className='font-poppins justify-center items-center flex h-screen'>
         <div className='mx-[40px] flex justify-center items-center gap-[30px]'>
             <div>
-                <h1 className='text-jblack text-[38px] font-bold'>Uncover Your Dream Job, Analyze Trends, Master In-Demand Skills with <span className='text-jgreen text-[34px] font-light'>Jobbo</span></h1>
-                <p className='text-jblack opacity-50 text-[16px] font-light my-[10px]'> Discover the hottest jobs in your industry or desired field. Jobbo analyzes job postings across various platforms to identify the most sought-after positions and equip you with the knowledge to make informed career choices.</p>
+                <h1 className='text-jblack text-[44px] font-bold'>Uncover Your Dream Job, Analyze Trends with <span className='text-jgreen text-[44px] font-bold'>Jobbo</span></h1>
+                <p className='text-jblack opacity-50 text-[14px] font-light my-[10px] leading-6'>Discover the hottest jobs in your industry or desired field. Jobbo analyzes job postings across various platforms to identify the most sought-after positions and equip you with the knowledge to make informed career choices.</p>
                 <SearchBarRepo hinttext='Get job analytics' buttontxt='Find a Job' inputclass='h-[50px] w-[400px]' buttonclass='h-[50px] w-[120px]' formclass='my-[40px]' value={userJobInput} onchange={(e: any) => setuserJobInput(e.target.value)} submit={handleSubmit} />
             </div>
             <div >
-                <img src={img} alt="heroimage" height={1400} width={1400}/>
+                <img src={img} alt="heroimage" height={1800} width={1800}/>
             </div>
         </div>
     </div>
--- src/components/interface/SearchBarRepo.tsx
+++ src/components/interface/SearchBarRepo.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image';
 
 interface Props {
     hinttext: string,
@@ -15,8 +16,13 @@ const SearchBarRepo = ({hinttext, buttontxt, inputclass, buttonclass, submit, va
   return (
     <div>
         <form onSubmit={submit} className={`flex items-center gap-[30px] ${formclass}`}>
-            <input type="text" placeholder={hinttext} value={value} onChange={onchange} className={`bg-jsgrey text-[12px] text-jblack font-light font-poppins outline-none rounded-md pl-[10px] ${inputclass}`} />
-            <button type='submit' disabled={value === ''} className={`bg-jblack rounded-md text-[12px] font-poppins font-semibold text-jwhite ${buttonclass}`}>{buttontxt}</button>
+            <div className='relative'>
+                <input type="text" placeholder={hinttext} value={value} onChange={onchange} className={`bg-jsgrey text-[12px] text-jblack font-normal font-poppins outline-none rounded-lg pl-[40px] ${inputclass}`} />
+                <div className='absolute flex items-center inset-y-0 left-0 pointer-events-none pl-[10px]'>
+                  <Image src="/search.svg" alt="search_svg" width={20} height={20} />
+                </div>
+            </div>
+            <button type='submit' disabled={value === ''} className={`bg-jblack rounded-lg text-[12px] font-poppins font-medium text-jwhite ${buttonclass}`}>{buttontxt}</button>
         </form>
     </div>
   )
