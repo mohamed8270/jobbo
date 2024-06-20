39d3d9b228f1a21ad6a465e0698720dee4069b77
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-20 11:00:58 +0530

    job analytics updated card and page building on progress

--- constants/static.tsx
+++ constants/static.tsx
@@ -1,7 +1,7 @@
 export const navLinks = [
     {path: '/', svg: '/home-outline.svg', svgfil:'/home-filled.svg', text: 'Home'},
     {path: '/JobsDataPage', svg: '/brief-outline.svg', svgfil:'/brief-filled.svg', text: 'Jobs'},
-    {path: '/analytics', svg: '/graph-outline.svg', svgfil:'/graph-filled.svg', text: 'Analytics'},
+    {path: '/AnalyticsDataPage', svg: '/graph-outline.svg', svgfil:'/graph-filled.svg', text: 'Analytics'},
     {path: '/scope', svg: '/scope-outline.svg', svgfil:'/scope-filled.svg', text: 'Scope'},
     {path: '/support-center', svg: '/support-outline.svg', svgfil:'/support-filled.svg', text: 'Support Center'},
     {path: '/settings', svg: '/settings-outline.svg', svgfil:'/settings-filled.svg', text: 'Settings'},
---
+++ src/components/JobsAnalyticsCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+    href: string,
+    icn: string,
+    txt1: string,
+    txt2: string,
+    percentage: string,
+}
+
+const JobsAnalyticsCard = ({href, icn, txt1, txt2, percentage} : Props) => {
+  return (
+    <Link href={href}>
+        <div className='h-[120px] w-[260px] bg-jwhite border-2 border-jsgrey rounded-lg font-poppins'>
+            <div className='flex flex-col justify-center items-start m-[10px]'>
+                <h1 className='text-jblack font-semibold text-[12px]'>{txt1}</h1>
+                <div className='flex gap-[10px]'>
+                    <Image src={icn} alt='logo' height={22} width={22} className='fill-jblack text-jblack'/>
+                    <h1 className='text-[22px] font-normal text-jblack my-[5px]'>{txt2}</h1>
+                </div>
+                <div className='flex justify-between items-center my-[7px] gap-[40px]'>
+                    <h1 className='text-[14px] font-normal text-jgreen'>{percentage}</h1>
+                    <p className='text-[12px] font-medium text-jblack text-opacity-20'>vs last week</p>
+                </div>
+            </div>
+        </div>
+    </Link>
+  )
+}
+
+export default JobsAnalyticsCard
