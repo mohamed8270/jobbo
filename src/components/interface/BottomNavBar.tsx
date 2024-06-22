f132013febcd567085671f69000718a0841a1469
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-22 16:58:39 +0530

    border updated with bottom nav bar

--- src/components/interface/BottomNavBar.tsx
+++ src/components/interface/BottomNavBar.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 const BottomNavBar = () => {
     const pathname = usePathname();
   return (
-    <div className='font-poppins bg-jwhite fixed bottom-0 z-50 left-0 h-[65px] w-full border-t border-jsgrey sm:visible md:hidden'>
+    <div className='font-poppins bg-jwhite fixed bottom-0 z-50 left-0 h-[65px] w-full border-t-2 border-jsgrey sm:visible md:hidden'>
         <div className='grid h-full max-w-full justify-items-center grid-cols-5 mx-auto justify-center items-center'>
             {bottomNavLinks.map((data) => (
                 <BottomButton href={data.path} btnclass='' spnclass={pathname === data.path ? 'font-bold text-jgreen' : 'font-semibold text-jblack'} txt={data.text} icn={pathname === data.path ? data.svgfil : data.svg} h={pathname === data.path ? 23 : 20} w={pathname === data.path ? 23 : 20} />
