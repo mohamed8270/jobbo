a598df82460bba4cac4ce93d4add2043f3f88dc2
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-27 18:03:42 +0530

    email content with #html5 and notification service updated

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -13,21 +13,20 @@ const Notification = {
 const readHTMLContent = async () => {
     const htmlFilePath = 'lib/nodemailer/html_content/welcome_email.html'; // Replace with actual path
 
-    await fse.readFile(htmlFilePath, 'utf8', (err: any, data: any) => {
-        if (err) {
-          console.error(err);
-          return data;
-        }
-    });
-
-    return 'Email Error';
+    try {
+        const data = await fse.readFile(htmlFilePath, 'utf8');
+        return data;
+    } catch (error) {
+        console.log(error);
+        return 'Email Error';
+    }
 }
 
 // mail type accordian
 export async function generateEmailBody(job: EmailJobInfo, type: NotificationType) {
 
     // shortened title
-    const shortenedTitle = job.jobName.length > 20 ? `${job.jobName.substring(0,20)}` : job.jobName;
+    const shortenedTitle = job.jobName.length > 26 ? `${job.jobName.substring(0,20)}` : job.jobName;
 
     // read HTML content
     const htmlRead = await readHTMLContent();
@@ -37,12 +36,13 @@ export async function generateEmailBody(job: EmailJobInfo, type: NotificationTyp
     // subject and title
     let subject = '';
     let body = htmlContent;
+    console.log(`this is body ${body}`);
 
     // switch case to generate body and sub
     switch (type) {
         case Notification.WELCOME:
-            subject = `Welcome to Code Pay 💰 for ${shortenedTitle}`;
-            body = ``;
+            subject = `Welcome to Jobbo 💰 for ${shortenedTitle}`;
+            body = htmlContent;
             break;
     
         default:
