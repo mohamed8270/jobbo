b1d43f80c285c44248b6e3f2a79cfa231536428e
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-27 10:16:06 +0530

    job data analytics UI #updated

--- app/AnalyticsDataPage/page.tsx
+++ app/AnalyticsDataPage/page.tsx
@@ -14,9 +14,9 @@ const Page = async () => {
   }));
 
   return (
-    <div className='h-screen bg-jbgrey pt-[80px] pl-[40px] pr-[40px] min-h-screen'>
+    <div className='h-screen bg-jbgrey pt-[80px] pl-[20px] pr-[20px] md:pl-[40px] md:pr-[40px] min-h-screen w-screen md:w-full'>
       <div className='pt-[40px]'>
-        <div className='flex justify-between items-center'>
+        <div className='sm:flex-row md:flex justify-between items-center'>
           <JobsAnalyticsCard href='/' icn='https://www.svgrepo.com/show/509920/eye.svg' txt1='Most viewed' txt2={extractJobviewsAverage(jobsdata)} txt3={extractJobviewsCount(jobsdata)} percentage='3.8%' />
           <JobsAnalyticsCard href='/' icn='https://www.svgrepo.com/show/533600/arrow-narrow-circle-broken-up-right.svg' txt1='High paid' txt2={extractJobHighpay(jobsdata)} txt3='' percentage='3.8%' />
           <JobsAnalyticsCard href='/' icn='https://www.svgrepo.com/show/533596/arrow-narrow-circle-broken-down-left.svg' txt1='Low paid' txt2={extractJoblowpay(jobsdata)} txt3='' percentage='3.8%' />
