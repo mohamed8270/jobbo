b293fda1e7665d6303b5b2a684746432bf4dd5e5
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-06-28 16:41:36 +0530

    nav bar width #updated and issue solving #progress

--- src/components/interface/NavBar.tsx
+++ src/components/interface/NavBar.tsx
@@ -6,12 +6,14 @@ const NavBar = () => {
   return (
     <header className='md:top-0 md:left-[260px] md:right-0 font-poppins bg-jwhite border-b-[2px] border-jsgrey fixed w-full'>
         <nav className='h-[80px] mx-auto flex items-center md:px-[40px] px-[20px] justify-between'>
-            <div className='md:hidden visible mr-[20px]'>
-              <Link href='/JobsDataPage'><Image src="/assets/icons/menu.svg" alt="menu_svg" width={25} height={25} className='inline-block cursor-pointer text-jblack' /></Link>
+            <div className='flex justify-start items-center'>
+              <div className='md:hidden visible mr-[15px]'>
+                <Link href='/JobsDataPage'><Image src="/assets/icons/menu.svg" alt="menu_svg" width={25} height={25} className='inline-block cursor-pointer text-jblack' /></Link>
+              </div>
+              <h1 className='text-jblack md:text-[16px] text-[13px] font-semibold'>Uncover your perfect job opportunity! 💼</h1>
             </div>
-            <h1 className='text-jblack text-[16px] font-semibold'>Uncover your perfect job opportunity! 💼</h1>
-            <div className='md:visible invisible'>
-              <Image src="/assets/icons/notify-outline.svg" alt="notify_svg" width={20} height={20} className='inline-block cursor-pointer text-jblack' />
+            <div className='md:visible visible'>
+              <Image src="/assets/icons/notify-outline.svg" alt="notify_svg" width={23} height={23} className='inline-block cursor-pointer text-jblack' />
             </div>
         </nav>
     </header>
