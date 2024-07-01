a48b1e4135259137a710753b568dbba16403a56b
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-07-01 14:41:14 +0530

    basic #UI changes in PWA structure

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -9,8 +9,9 @@ const page = async () => {
 
   return (
     <section className='flex flex-wrap flex-col gap-10'>
-        <div className='bg-jbgrey pt-[80px] px-[10px] md:px-[40px] min-h-screen'>
+        <div className='bg-jbgrey md:pt-[80px] pt-[50px] px-[10px] md:px-[40px] min-h-screen'>
           <SearchJobsData/>
+          <h1 className='text-sm md:text-lg font-poppins font-semibold text-jblack mb-[20px]'>Trending Jobs</h1>
           <div className='flex flex-wrap gap-x-12 md:gap-y-16 gap-y-6'>
             {jobsData?.map((jobs) => (
               <JobsDataCard key={jobs._id} jobs={jobs} />
